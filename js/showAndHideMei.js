83b8496a68e77c6044be80408fea08be5ccd2244
RossignolD <knightsoflogicland@gmail.com>
2022-11-27 12:26:37 -0500

    buttons on homepage working again, at cost of reversion of lastest commit

--- js/showAndHideMei.js
+++ js/showAndHideMei.js
@@ -1,8 +1,29 @@
-$(document).ready(function(){
-	$('.showbtn').click(function(){
-   		$('#hidemei').show();
+	$(document).ready(function () {
+        $("#image").hide();
+        $("#hide").attr('disabled', true);
+        $("#hide").click(function () {
+            $("#image").hide();
+            $("#hide").attr('disabled', true);
+            $("#show").attr('disabled', false);
+  
+        });
+        $("#show").click(function () {
+            $("#image").show();
+            $("#hide").attr('disabled', false);
+            $("#show").attr('disabled', true);
+        });
+    });
+	
+	$("#image").hide();
+	attr('disabled', true);
+	$("#hide").click(function () {
+		$("#image").hide();
+		$("#hide").attr('disabled', true);
+		$("#show").attr('disabled', false);
+
 	});
-  	$('.hidebtn').click(function(){
-   		$('#hidemei').hide();
+	$("#show").oclick(function () {
+		$("#image").show();
+		$("#hide").attr('disabled', false);
+		$("#show").attr('disabled', true);
 	});
-});
