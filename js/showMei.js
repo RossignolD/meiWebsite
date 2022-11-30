977b23cec69b8fbe3b1707c844ba903ba9a23ddd
RossignolD <meirconnor@gmail.com>
2022-11-30 14:05:24 -0500

    button on homepage now working, pronuciation of name removed

---
+++ js/showMei.js
@@ -0,0 +1,8 @@
+function hideMei(){
+    const Mei = document.getElementById("meipic");
+  if (Mei.style.display === "none") {
+    Mei.style.display = "block";
+  } else {
+    Mei.style.display = "none";
+  }
+}
