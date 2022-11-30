879355e27516a98f60feddd92248b41ce53d20bd
RossignolD <meirconnor@gmail.com>
2022-11-30 14:50:16 -0500

    image on homepage now hidden until DOMContentLoaded

--- js/showMei.js
+++ js/showMei.js
@@ -1,8 +1,15 @@
+document.addEventListener("DOMContentLoaded",
+  function hide(){
+    const Mei = document.getElementById("meipic");
+    Mei.style.display=(Mei.style.display==="none") ? '': "none";
+  }
+);
+
 function hideMei(){
     const Mei = document.getElementById("meipic");
-  if (Mei.style.display === "none") {
-    Mei.style.display = "block";
-  } else {
+  if (Mei.style.display === "block") {
     Mei.style.display = "none";
+  } else {
+    Mei.style.display = "block";
   }
-}
+};
